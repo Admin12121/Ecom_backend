c3d1295b42246917a5ca73b141d6fb6def42de26
Admin12121 <vickytaj6459@gmail.com>
2024-12-20 22:37:00 +0545

    webhook url updated

--- sales/urls.py
+++ sales/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import SalesViewSet, RedeemCodeViewSet
+from .views import SalesViewSet, RedeemCodeViewSet, StripeWebhookView as webhook
 
 router = DefaultRouter()
 router.register(r'sales', SalesViewSet, basename='sales')
@@ -8,5 +8,6 @@ router.register(r'redeemcode', RedeemCodeViewSet, basename='redeem-code')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('webhook/', webhook.as_view() , name='stripe-webhook'),
     path('sales/transaction/<str:transactionuid>/', SalesViewSet.as_view({'get': 'retrieve'}), name='sales-detail'),
 ]
